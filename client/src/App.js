1107067a2afd6e98b4ab6e60351e335c2d75a587
Cydney Pollard <sincerelycydney087@gmail.com>
2019-10-19 19:21:31 -0400

    added home header and bulma

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar.js';
+import Home from './components/homeheader.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
       hey world!
     </div>
   );
---
+++ client/src/components/homeheader.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function HomeHeader() {
+    return (
+        <div>
+            <section className="hero is-large has-bg-img">
+            <div className="hero-body">
+                <div className="container">
+                <h1 className="title text-center text-white">
+                    WELCOME TO FEMINAKU!
+                </h1>
+                <h2 className="subtitle text-center text-white">
+                    Women and Anime Make a Bomb Combo!
+                </h2>
+                </div>
+            </div>
+            </section>
+        </div>
+    )
+}
+
+export default HomeHeader;
