9fbb4b9f12dfd6b5221f9f47bf5bfc7ff85792cd
Cydney Pollard <sincerelycydney087@gmail.com>
2019-10-20 10:29:05 -0400

    attempt at fetch product shops from backend

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -3,12 +3,12 @@ import React from 'react';
 function About() {
     return (
         <div>
-            <section class="hero is-medium is-dark is-bold">
-            <div class="hero-body">
-                <h1 class="header-title">
+            <section className="hero is-medium is-dark is-bold">
+            <div className="hero-body">
+                <h1 className="header-title">
                     ALL ABOUT US..
                 </h1>
-                <h2 class="header-subtitle">
+                <h2 className="header-subtitle">
                 Feminaku was founded in 2018 and is an upcoming social networking application 
                 for women who love all things anime. Feminaku is a combination 
                 of the word feminine and otaku, a Japanese term for people with obsessive 
--- client/src/components/reviews.js
+++ client/src/components/reviews.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import axios from 'axios';
 
-function Contact() {
-    return (
+class Contact extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            productShops: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('/api/products')
+        .then(res => {
+            console.log(res)
+            this.setState( {productShops: res.data})
+        })
+    }
+
+    render() {
+        return (
         <div>
         <section className="hero is-large has-bg-img3">
-             Yo
+             <ul>
+                 
+                 { this.state.productShops.map(productShop => <li>{productShop.description}</li>)}
+             </ul>
         </section>
         </div>
     )
+    }
+    
 }
 
 export default Contact;
