c82726010f9aaded52a55e3a951077d3e613bea1
Cydney Pollard <sincerelycydney087@gmail.com>
2019-10-21 11:36:57 -0400

    fetch request for users

--- client/src/components/users.js
+++ client/src/components/users.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Axios from 'axios';
 
 class Reviews extends React.Component {
 
@@ -6,14 +7,25 @@ class Reviews extends React.Component {
         super(props)
 
         this.state = {
-            reviews: []
+            users: []
         }
+    }
+
+    componentDidMount() {
+        Axios.get('/api/user')
+        .then(res => {
+            console.log(res)
+            this.setState({ users: res.data })
+        })
     }
     render(){
         return(
         <div>
             <section className="hero is-large has-bg-img2">
-                You
+            <div className="header-title2">User Spotlight</div>
+            <ul>
+        { this.state.users.map(user => <li>{user.name}</li>)}
+      </ul>
             </section>
         </div>
         )
